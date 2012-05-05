2d84636a07d2bb90628ed39c71575952e377fc11
Michiel de Jong <michiel@unhosted.org>
2012-05-05 12:08:14 +0200

    example contact

---
+++ beispiel.js
@@ -0,0 +1,10 @@
+var addressBook = {
+  'mailto:michiel@unhosted.org': {
+    rel: 'work',
+    sameAs: ['irc:michielbdejong@freenode.net'],
+    attr: {
+      firstName: 'Michiel',
+      lastName: 'de Jong'
+    }
+  }
+};
